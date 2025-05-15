d83c7f947109f767f4e6de22152a8a0ebd6de8d4
YanisLT <lefortyanis27@gmail.com>
2025-05-15 12:43:13 +0200

    some more things - added healthbar lerp - added spin def for entity (not sure if thats the way to do it) - todo: better controller handling, dying animation, etc

--- pygarras.py
+++ pygarras.py
@@ -1,6 +1,7 @@
 import math
 import pygame
 import sys
+import random
 
 # Constants
 
@@ -17,7 +18,7 @@ COL_YELLOW = (255, 255, 0)
 
 # settings
 
-window_dimensions = (1920, 1080)  # in pixels
+window_dimensions = (2560, 1440)  # in pixels
 frames_per_second = 120
 
 # Parameters
@@ -53,15 +54,16 @@ class Entity:
         self.y = y
         self.size = size
         self.shape = shape
-        self.vx = 0
-        self.vy = 0
-        self.ax = 0
-        self.ay = 0
+        self.vx = 0.0
+        self.vy = 0.0
+        self.ax = 0.0
+        self.ay = 0.0
         self.col = col
         self.health = health
         self.max_health = health
+        self.old_health_percentage = 1.0
         self.alive = True
-        self.angle = 0
+        self.angle = 0.0
         self.guns = []
     
     def kill(self):
@@ -76,6 +78,13 @@ class Entity:
 
     def heal(self, amount):
         self.health = min(self.health + amount, self.max_health)
+
+    def spin(self, angle):
+        self.angle += angle
+        if self.angle > 2 * math.pi:
+            self.angle -= 2 * math.pi
+        elif self.angle < -2 * math.pi:
+            self.angle += 2 * math.pi
 
 def is_targeted(entity, mouse_pos):
     difference = math.sqrt((entity.x - mouse_pos[0]) ** 2 + (entity.y - mouse_pos[1]) ** 2)
@@ -100,9 +109,11 @@ def manage_inputs(input_type):
                     entity.kill()
         if event.key == pygame.K_f:
             new_entity = Entity(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1], 20, 0, COL_RED, 100)
-            new_entity.guns.append(Gun(15, 10, -1.4, 0, 0, 0, COL_GREY))
-            new_entity.guns.append(Gun(15, 10, -1.4, 0, 0, 120, COL_GREY))
-            new_entity.guns.append(Gun(15, 10, -1.4, 0, 0, 240, COL_GREY))
+            new_entity.guns.append(Gun(18, 8, 1, 0, 0, 0, COL_GREY))
+            new_entity.guns.append(Gun(14, 8, 1, 0, -1, 140, COL_GREY))
+            new_entity.guns.append(Gun(14, 8, 1, 0, 1, -140, COL_GREY))
+            new_entity.guns.append(Gun(16, 8, 1, 0, 0, 150, COL_GREY))
+            new_entity.guns.append(Gun(16, 8, 1, 0, 0, -150, COL_GREY))
             entities.append(new_entity)
         if event.key == pygame.K_q:
             for entity in entities[:]:
@@ -124,30 +135,30 @@ def draw_grid():
 
 def draw_guns(entity):
     for gun in entity.guns:
-        a = entity.angle + gun.angle
-        length = gun.length * 2
-        width = gun.width
+        angle = entity.angle + gun.angle
+        length = gun.length * 2 * entity.size/20
+        width = gun.width * entity.size/20
         darker = (gun.col[0] // 2, gun.col[1] // 2, gun.col[2] // 2)
 
-        newAspect = gun.aspect if gun.aspect > 1 else gun.aspect if 0<gun.aspect<=1 else 1
-        newAspect2 = abs(gun.aspect) if gun.aspect < -1 else abs(gun.aspect) if -1<gun.aspect<=0 else 1
+        aspect = gun.aspect if gun.aspect > 1 else gun.aspect if 0<gun.aspect<=1 else 1
+        aspect2 = abs(gun.aspect) if gun.aspect < -1 else abs(gun.aspect) if -1<gun.aspect<=0 else 1
 
-        offset_x = gun.x * math.cos(a) - gun.y * math.sin(a)
-        offset_y = gun.x * math.sin(a) + gun.y * math.cos(a)
+        offset_x = gun.x * math.cos(angle) - gun.y * math.sin(angle)
+        offset_y = gun.x * math.sin(angle) + gun.y * math.cos(angle)
         base_x = entity.x + offset_x * 2
         base_y = entity.y + offset_y * 2
 
-        p1x = base_x + width * math.sin(a) * newAspect2
-        p1y = base_y - width * math.cos(a) * newAspect2
+        p1x = base_x + width * math.sin(angle) * aspect2
+        p1y = base_y - width * math.cos(angle) * aspect2
 
-        p2x = base_x - width * math.sin(a) * newAspect2
-        p2y = base_y + width * math.cos(a) * newAspect2
+        p2x = base_x - width * math.sin(angle) * aspect2
+        p2y = base_y + width * math.cos(angle) * aspect2
 
-        p3x = base_x + length * math.cos(a) - width * math.sin(a) * newAspect
-        p3y = base_y + length * math.sin(a) + width * math.cos(a) * newAspect
+        p3x = base_x + length * math.cos(angle) - width * math.sin(angle) * aspect
+        p3y = base_y + length * math.sin(angle) + width * math.cos(angle) * aspect
 
-        p4x = base_x + length * math.cos(a) + width * math.sin(a) * newAspect
-        p4y = base_y + length * math.sin(a) - width * math.cos(a) * newAspect
+        p4x = base_x + length * math.cos(angle) + width * math.sin(angle) * aspect
+        p4y = base_y + length * math.sin(angle) - width * math.cos(angle) * aspect
 
         # Define the polygon for the gun barrel
         polygon = [(p1x, p1y), (p2x, p2y), (p3x, p3y), (p4x, p4y)]
@@ -196,16 +207,21 @@ def draw_hp_bar(entity):
     outline_width = 5
     bar_width = 3
     if entity.alive  and entity.health < entity.max_health:
+        health_percentage = entity.health / entity.max_health
+        entity.old_health_percentage += (health_percentage - entity.old_health_percentage) * 0.3
+
+
         pygame.draw.rect(window, COL_BLACK, (entity.x - entity.size - outline_width/2, entity.y + entity.size + 10 - outline_width/2, entity.size * 2 + outline_width, bar_width + outline_width), 0, 10)
-        pygame.draw.rect(window, COL_GREEN, (entity.x - entity.size, entity.y + entity.size + 10, entity.size * 2 * ((entity.health / entity.max_health)), bar_width),0,10)
+        pygame.draw.rect(window, COL_GREEN, (entity.x - entity.size, entity.y + entity.size + 10, entity.size * 2 * (entity.old_health_percentage), bar_width),0,10)
 
 
 player = Entity(initial_position[0], initial_position[1], 20, 0, COL_BLUE, 100)
-player.guns.append(Gun(18, 10, -1.4, 0, 0, 0, COL_GREY))
+player.guns.append(Gun(32, 8, 1, 0, 0, 0, COL_GREY))
+player.guns.append(Gun(5, 8, -1.4, 8, 0, 0, COL_GREY))
 entities.append(player)
 
 while True:
-    temps_maintenant = pygame.time.get_ticks()
+    time_now = pygame.time.get_ticks()
     window.fill(COL_BACKGROUND)
     draw_grid()
     time = pygame.time.Clock()
@@ -216,7 +232,8 @@ while True:
     for entity in entities:
         if entity.alive:
             draw_entity(entity)
-            #player.size += 0.1
+            #entity.size += 0.1
+            entity.spin(random.uniform(0,.8))
             #player.guns[0].aspect += 0.01
             player.angle = math.atan2(pygame.mouse.get_pos()[1] - player.y , pygame.mouse.get_pos()[0] - player.x)
             # Update entity position based on speed and angle
